57dc6c19e59718a9fb6b11f49f31ca7c20713a89
Ceyhun Cengiz <ceyhun_cengiz@windowslive.com>
2022-02-10 00:10:04 +0300

    issue fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-import Particles from 'react-tsparticles'
-import Clarifai from 'clarifai'
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import Rank from './components/Rank/Rank'
-import './App.css'
+import React, { Component } from "react";
+import Particles from "react-tsparticles";
+import Clarifai from "clarifai";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
 
 const app = new Clarifai.App({
-  apiKey: '222d66483825430fa1f021cfcd3d2432',
-})
+  apiKey: "222d66483825430fa1f021cfcd3d2432",
+});
 
 const particlesOptions = {
   particles: {
@@ -18,43 +18,45 @@ const particlesOptions = {
       enable: true,
       value_area: 1000,
     },
+    move: {
+      enable: true,
+    },
   },
-}
+};
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      input: '',
-    }
+      input: "",
+    };
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value)
-  }
+    console.log(event.target.value);
+  };
 
   onButtonSubmit = () => {
-    console.log('click')
+    console.log("click");
     app.models
       .predict(
-        '45fb9a671625463fa646c3523a3087d5',
-        'https://samples.clarifai.com/metro-north.jpg'
+        "45fb9a671625463fa646c3523a3087d5",
+        "https://samples.clarifai.com/metro-north.jpg"
       )
       .then(
         function (response) {
-          console.log(response)
+          console.log(response);
         },
         function (err) {
           //
         }
-      )
-  }
+      );
+  };
 
   render() {
     return (
       <div className="App">
         <Particles id="particles" params={particlesOptions} />
-
         <Navigation />
         <Logo />
         <Rank />
@@ -65,8 +67,8 @@ class App extends Component {
 
         {/*<FaceRecognition />*/}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
